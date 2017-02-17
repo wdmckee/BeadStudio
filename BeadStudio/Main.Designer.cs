8b38a4777a3af2c92be3be36a4e20485465e19fb
wdmckee <derek.mckee@29609-SP4>
2017-02-16 22:56:27 -0600

    initial

---
+++ BeadStudio/Main.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace BeadStudio
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox_Colors = new System.Windows.Forms.PictureBox();
+            this.print_btn = new System.Windows.Forms.Button();
+            this.checkBox_Gridlines = new System.Windows.Forms.CheckBox();
+            this.checkBox_Pixelated = new System.Windows.Forms.CheckBox();
+            this.Load_Picture = new System.Windows.Forms.Button();
+            this.pictureBoxImg = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Colors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox_Colors);
+            this.splitContainer1.Panel1.Controls.Add(this.print_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox_Gridlines);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox_Pixelated);
+            this.splitContainer1.Panel1.Controls.Add(this.Load_Picture);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBoxImg);
+            this.splitContainer1.Size = new System.Drawing.Size(1794, 1767);
+            this.splitContainer1.SplitterDistance = 300;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox_Colors
+            // 
+            this.pictureBox_Colors.Location = new System.Drawing.Point(26, 300);
+            this.pictureBox_Colors.Name = "pictureBox_Colors";
+            this.pictureBox_Colors.Size = new System.Drawing.Size(180, 600);
+            this.pictureBox_Colors.TabIndex = 10;
+            this.pictureBox_Colors.TabStop = false;
+            this.pictureBox_Colors.Click += new System.EventHandler(this.pictureBox_Colors_Click);
+            // 
+            // print_btn
+            // 
+            this.print_btn.Location = new System.Drawing.Point(26, 231);
+            this.print_btn.Name = "print_btn";
+            this.print_btn.Size = new System.Drawing.Size(174, 45);
+            this.print_btn.TabIndex = 9;
+            this.print_btn.Text = "Print";
+            this.print_btn.UseVisualStyleBackColor = true;
+            this.print_btn.Click += new System.EventHandler(this.print_btn_Click);
+            // 
+            // checkBox_Gridlines
+            // 
+            this.checkBox_Gridlines.AutoSize = true;
+            this.checkBox_Gridlines.Location = new System.Drawing.Point(26, 148);
+            this.checkBox_Gridlines.Name = "checkBox_Gridlines";
+            this.checkBox_Gridlines.Size = new System.Drawing.Size(170, 29);
+            this.checkBox_Gridlines.TabIndex = 8;
+            this.checkBox_Gridlines.Text = "Map to Pallet";
+            this.checkBox_Gridlines.UseVisualStyleBackColor = true;
+            this.checkBox_Gridlines.CheckedChanged += new System.EventHandler(this.checkBox_Gridlines_CheckedChanged);
+            // 
+            // checkBox_Pixelated
+            // 
+            this.checkBox_Pixelated.AutoSize = true;
+            this.checkBox_Pixelated.Location = new System.Drawing.Point(26, 103);
+            this.checkBox_Pixelated.Name = "checkBox_Pixelated";
+            this.checkBox_Pixelated.Size = new System.Drawing.Size(192, 29);
+            this.checkBox_Pixelated.TabIndex = 6;
+            this.checkBox_Pixelated.Text = "Show Pixelated";
+            this.checkBox_Pixelated.UseVisualStyleBackColor = true;
+            this.checkBox_Pixelated.CheckedChanged += new System.EventHandler(this.checkBox_Pixelated_CheckedChanged);
+            // 
+            // Load_Picture
+            // 
+            this.Load_Picture.Location = new System.Drawing.Point(26, 40);
+            this.Load_Picture.Name = "Load_Picture";
+            this.Load_Picture.Size = new System.Drawing.Size(174, 45);
+            this.Load_Picture.TabIndex = 0;
+            this.Load_Picture.Text = "Picture";
+            this.Load_Picture.UseVisualStyleBackColor = true;
+            this.Load_Picture.Click += new System.EventHandler(this.Load_Picture_Click);
+            // 
+            // pictureBoxImg
+            // 
+            this.pictureBoxImg.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBoxImg.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxImg.Name = "pictureBoxImg";
+            this.pictureBoxImg.Size = new System.Drawing.Size(1120, 1120);
+            this.pictureBoxImg.TabIndex = 0;
+            this.pictureBoxImg.TabStop = false;
+            this.pictureBoxImg.Click += new System.EventHandler(this.pictureBoxImg_Click);
+            this.pictureBoxImg.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxImg_Paint);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1794, 1767);
+            this.Controls.Add(this.splitContainer1);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = " Bead Studio";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Colors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBoxImg;
+        private System.Windows.Forms.Button Load_Picture;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.CheckBox checkBox_Pixelated;
+        private System.Windows.Forms.CheckBox checkBox_Gridlines;
+        private System.Windows.Forms.Button print_btn;
+        private System.Windows.Forms.PictureBox pictureBox_Colors;
+    }
+}
+
---
+++ BeadStudio/Main.cs
@@ -0,0 +1,486 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeadStudio
+{
+    public partial class Main : Form
+    {
+
+        #region enums
+        public enum CurrentState { FullSize, Zoomed, Pixelated, ColorMapped, Gridlines  }
+        #endregion
+
+        #region fields
+        string currentfile;
+        Bitmap currentFullSizeBitmap;
+        Bitmap current28x28SizeBitmap;
+        Bitmap currentPixelatedBitmap;
+        Bitmap currentPixelatedColorMappedBitmap;
+        Bitmap currentPixelatedColorMappedBitmapWithGridlines;
+
+        bool eventInQueue = false;      
+
+        Color[] ColorArray =
+               {
+            Color.FromArgb(255, 196, 36, 27),  // red
+            Color.FromArgb(255, 202, 73, 136),  // pink
+            Color.FromArgb(255, 91, 39, 140),  // purple
+            Color.FromArgb(255, 12, 25, 104),  // navy blue
+            Color.FromArgb(255, 26, 75, 152),  // royal blue
+            Color.FromArgb(255, 20, 110, 59),  //dark green
+            Color.FromArgb(255, 53, 29, 19),  // brown
+            Color.FromArgb(255, 102, 102, 102),  // gray
+            Color.FromArgb(255, 0, 0, 0),  // black
+            Color.FromArgb(255, 197, 137, 109),  // tan
+            Color.FromArgb(255, 148, 130, 190),  // purple
+            Color.FromArgb(255, 249, 176, 68),  // orange
+            Color.FromArgb(255, 136, 192, 218),  // baby blue
+            Color.FromArgb(255, 96, 187, 47),  // light green
+            Color.FromArgb(255, 255, 255, 255),  // white
+            Color.FromArgb(255, 229, 197, 53),  // yellow
+            Color.FromArgb(255, 225, 93, 21),  // ?
+            // THESE ARE OUR BASE COLORS
+            
+        };
+        #endregion
+
+        #region Properties
+        public CurrentState _currentState { get; set; }
+        #endregion        
+
+        #region METHODS
+        private void GetFullSizeImage(string file)
+        {
+
+
+            using (Stream BitmapStream = System.IO.File.Open(file, System.IO.FileMode.Open))
+            {
+                Image img = Image.FromStream(BitmapStream);
+
+                Bitmap mBitmap = new Bitmap(img);
+                //mBitmap = Helpers.ResizeImage(mBitmap, width, height);
+                FileInfo fi = new FileInfo(file);
+                mBitmap.Tag = fi.Name;
+                currentFullSizeBitmap = mBitmap;
+                current28x28SizeBitmap = currentFullSizeBitmap.ResizeImage(28, 28);
+                currentPixelatedBitmap = current28x28SizeBitmap.Pixelate(null, 280,280);
+                currentPixelatedColorMappedBitmap = current28x28SizeBitmap.Pixelate(ColorArray,280,280);
+                currentPixelatedColorMappedBitmapWithGridlines = currentPixelatedColorMappedBitmap.DrawGridlines();
+            }
+
+
+
+
+
+
+
+        }
+
+        #endregion
+        
+        #region EVENT CLICKS
+
+        private void pictureBoxImg_Paint(object sender, PaintEventArgs e)
+        {
+
+
+            if (eventInQueue)
+            {
+
+
+                // (1) UNHOOK ALL EVENTS, CLEAR ALL CHECKBOXES
+                //checkBox_Resized.CheckedChanged -= new System.EventHandler(this.checkBox_Resized_CheckedChanged);
+                checkBox_Pixelated.CheckedChanged -= new System.EventHandler(this.checkBox_Pixelated_CheckedChanged);
+                //checkBox_ColorMap.CheckedChanged -= new System.EventHandler(this.checkBox_ColorMap_CheckedChanged);
+                checkBox_Gridlines.CheckedChanged -= new System.EventHandler(this.checkBox_Gridlines_CheckedChanged);
+
+                //checkBox_Resized.Checked = false;
+                checkBox_Pixelated.Checked = false;
+                //checkBox_ColorMap.Checked = false;
+                checkBox_Gridlines.Checked = false;
+
+
+
+                // (2) DO OUR LOGIC
+                if (_currentState == CurrentState.Zoomed)
+                {
+                    pictureBoxImg.Image = current28x28SizeBitmap;
+                    //checkBox_Resized.Checked = true;
+                }
+                else if (_currentState == CurrentState.Pixelated)
+                {
+                    pictureBoxImg.Image = currentPixelatedBitmap;
+                    checkBox_Pixelated.Checked = true;
+                }
+                else if (_currentState == CurrentState.ColorMapped)
+                {
+                    pictureBoxImg.Image = currentPixelatedColorMappedBitmap;
+                    //checkBox_ColorMap.Checked = true;
+                }
+                else if (_currentState == CurrentState.Gridlines)
+                {
+                    pictureBoxImg.Image = currentPixelatedColorMappedBitmapWithGridlines;
+                    checkBox_Gridlines.Checked = true;
+                }
+                else
+                {
+                    pictureBoxImg.Image = currentFullSizeBitmap;
+                }
+
+
+                // (3) ENABLE ALL EVENTS AGAIN
+                //checkBox_Resized.CheckedChanged += new System.EventHandler(this.checkBox_Resized_CheckedChanged);
+                checkBox_Pixelated.CheckedChanged += new System.EventHandler(this.checkBox_Pixelated_CheckedChanged);
+                //checkBox_ColorMap.CheckedChanged += new System.EventHandler(this.checkBox_ColorMap_CheckedChanged);
+                checkBox_Gridlines.CheckedChanged += new System.EventHandler(this.checkBox_Gridlines_CheckedChanged);
+
+                eventInQueue = false;
+
+
+            }
+
+        }
+        private void Load_Picture_Click(object sender, EventArgs e)
+        {
+
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentfile = openFileDialog.FileName;
+                    GetFullSizeImage(currentfile);
+                }
+            }
+            pictureBoxImg.Image = currentFullSizeBitmap;
+            pictureBoxImg.SizeMode = PictureBoxSizeMode.StretchImage;
+           
+        }
+        private void FillColorBox(Color[] colorArray)
+        {
+            var tempColorArray = colorArray;  // holds the colors we have already filled
+            // NOTE: X & Y REVERSED
+
+            var mBitmap = new Bitmap(9, 30); // currently we support up to 300 colors
+            for (int _y = 0; _y < mBitmap.Height; _y++)
+            {
+                for (int _x = 0; _x < mBitmap.Width; _x++)
+                {
+                    if (tempColorArray.Length > 0)
+                    { 
+                        var pixel_x = (_x == 0 ? _x : _x - 1); // these allow us to get the pixel we just set 
+                        pixel_x = (_x == 0 && _y > 0 ? 8 : pixel_x); // did we drop to the next line?
+                        var pixel_y = (_x == 0 && _y > 0 ? _y-1 : _y);
+
+                        Color originalColor = mBitmap.GetPixel(pixel_x, pixel_y);
+                        var newColor = originalColor.GetClosestColor(tempColorArray);
+                        tempColorArray = tempColorArray.Where(c => c != newColor).ToArray();  // removes color from array so we don't use it again
+                        mBitmap.SetPixel(_x, _y, newColor);
+                       
+                    }
+                    else
+                    {
+                        mBitmap.SetPixel(_x, _y, Color.Transparent);   //splitContainer1.BackColor                                            
+                    }
+                }
+             
+            }
+
+
+            pictureBox_Colors.Image = mBitmap.Pixelate(colorArray, 180, 600);
+            //pictureBox_Colors.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+
+
+        private void checkBox_Resized_CheckedChanged(object sender, EventArgs e)
+        {
+            // THIS METHOD IS NO LONGER BEING USED ON THE UI. JUST TOO CONFUSING TO A USER.
+            /*
+            if (checkBox_Resized.Checked)
+            {
+                _currentState = CurrentState.Zoomed;
+            }
+            else
+            {
+                _currentState = CurrentState.FullSize;
+            }
+            eventInQueue = true;
+            this.Refresh();
+           */
+        }
+        private void checkBox_Pixelated_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_Pixelated.Checked)
+            {
+                _currentState = CurrentState.Pixelated;
+            }
+            else
+            {
+                _currentState = CurrentState.FullSize;
+            }
+            eventInQueue = true;
+            this.Refresh();
+           
+        }
+        private void checkBox_ColorMap_CheckedChanged(object sender, EventArgs e)
+        {
+            // THIS METHOD HAS BEEN MERGED WITH checkBox_Gridlines_CheckedChanged AND IS NOT USED ON THE UI
+            /*
+            if (checkBox_ColorMap.Checked)
+            {
+                _currentState = CurrentState.ColorMapped;
+            }
+            else
+            {
+                _currentState = CurrentState.FullSize;
+            }
+            eventInQueue = true;
+            this.Refresh();
+           */
+        }
+        private void checkBox_Gridlines_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_Gridlines.Checked)
+            {
+                _currentState = CurrentState.Gridlines;
+            }
+            else
+            {
+                _currentState = CurrentState.FullSize;
+            }
+            eventInQueue = true;
+            this.Refresh();
+        }
+
+
+        private void print_btn_Click(object sender, EventArgs e)
+        {
+
+            System.Drawing.Printing.PrintDocument myPrintDocument1 = new System.Drawing.Printing.PrintDocument();
+
+            PrintDialog myPrinDialog1 = new PrintDialog();
+
+            myPrintDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(printDocument_PrintPage);
+
+            myPrinDialog1.Document = myPrintDocument1;
+
+            if (myPrinDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myPrintDocument1.Print();
+            }
+
+
+
+        }
+        private void printDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(pictureBoxImg.Image, 0, 0);
+
+        }
+        private void pictureBox_Colors_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PALLET File (.pallet)|*.pallet";
+
+            using (openFileDialog)
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var file = openFileDialog.FileName;
+                   
+                    var lines = File.ReadAllLines(file).Select(a => a.Split(',')).ToArray();
+
+                    Color[] _ColorArray = new Color[lines.Count()+1]; // plus 1 since we alway sadd a transparent color
+        
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+
+                        int a = int.Parse(lines[i][0]);
+                        int r = int.Parse(lines[i][1]);
+                        int g = int.Parse(lines[i][2]);
+                        int b = int.Parse(lines[i][3]);
+
+                        
+                        _ColorArray[i] = Color.FromArgb(a, r, g, b);
+                    }
+                    _ColorArray[lines.Count()] = Color.Transparent;
+
+                    ColorArray = _ColorArray;
+                    FillColorBox(ColorArray);
+
+                }
+            }
+
+            GetFullSizeImage(currentfile);
+            eventInQueue = true;
+            this.Refresh();
+            // reload & set image data
+
+            // HERE WE WOULD LOAD AND PROCESS A COLOR FILE
+        }
+        private void pictureBoxImg_Click(object sender, EventArgs e)
+        {
+            // TO BE IMPLEMENTED VIA DRAG AND DROP
+            //if (_currentState == CurrentState.Gridlines)
+            //{
+            //    MouseEventArgs me = (MouseEventArgs)e;
+            //    Point coordinates = me.Location;
+
+            //    Color _Color = currentPixelatedColorMappedBitmap.GetPixel(coordinates.X, coordinates.Y);
+            //}
+        }
+        #endregion
+
+
+
+        public Main()
+        {
+            InitializeComponent();
+            this.Width = 700;
+            this.Height = 625;
+
+            System.Reflection.Assembly _Assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var a = _Assembly.GetManifestResourceNames();
+            Stream str = _Assembly.GetManifestResourceStream("BeadStudio.logo_app3.png");
+            pictureBoxImg.Image = new Bitmap(str);
+            pictureBoxImg.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            FillColorBox(ColorArray);
+        }
+
+       
+    }
+
+
+
+
+    static class Helpers
+    {
+
+        public static Bitmap ResizeImage(this Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        public static Bitmap Pixelate(this Image image, Color[] colorArray, int width, int height)
+        {
+            //currentPixelatedBitmap
+            Bitmap mBitmap = (Bitmap)image;
+
+
+            Bitmap PixelatedBitmap = new Bitmap(width, height);  //280, 280
+
+
+
+
+            for (int x = 0; x < mBitmap.Width; x++)
+            {
+                for (int y = 0; y < mBitmap.Height; y++)
+                {
+
+                    //get the pixel from the original image
+                    Color originalColor = mBitmap.GetPixel(x, y);
+
+                    if (colorArray != null) // if we pass in a color array, use it!
+                    {
+                        originalColor = originalColor.GetClosestColor(colorArray);
+                    }
+
+                    for (int xx = (x * 10); xx < (x * 10) + 10; xx++)
+                    {
+                        for (int yy = (y * 10); yy < (y * 10) + 10; yy++)
+                        {
+                            PixelatedBitmap.SetPixel(xx, yy, originalColor);
+                        }
+
+                    }
+                    //expandY
+
+                }
+            }
+
+
+
+            return PixelatedBitmap;
+
+
+        }
+
+        public static Bitmap DrawGridlines(this Image image)
+        {
+
+            Bitmap mBitmap = (Bitmap)image;
+            Bitmap mBitmapReturn = new Bitmap(mBitmap);
+
+
+            Color originalColor = Color.LightGray;
+
+            for (int x = 0; x < mBitmap.Width; x++)
+            {
+                for (int y = 0; y < mBitmap.Height; y++)
+                {
+                    if (x % 10 == 0 || y % 10 == 0) 
+                        mBitmapReturn.SetPixel(x, y, originalColor);
+                }
+            }
+
+            
+            return mBitmapReturn;
+        }
+
+        public static Color GetClosestColor(this Color baseColor ,Color[] colorArray)
+        {
+            var colors = colorArray.Select(x => new { Value = x, Diff = GetDiff(x, baseColor) }).ToList();
+            var min = colors.Min(x => x.Diff);
+            return colors.Find(x => x.Diff == min).Value;
+        }
+
+        public static int GetDiff(Color color, Color baseColor)
+        {
+            int a = color.A - baseColor.A,
+                r = color.R - baseColor.R,
+                g = color.G - baseColor.G,
+                b = color.B - baseColor.B;
+            return a * a + r * r + g * g + b * b;
+        }
+
+       
+
+
+
+    }
+
+
+
+
+    }
